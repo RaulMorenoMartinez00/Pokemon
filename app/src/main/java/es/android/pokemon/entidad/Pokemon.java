503102547020d4b06785b03c459d9ac716ddd6f8
Raul Piotr <rmm2110@gmail.com>
2022-04-27 11:39:55 +0200

    Initial commit

---
+++ app/src/main/java/es/android/pokemon/entidad/Pokemon.java
@@ -0,0 +1,43 @@
+package es.android.pokemon.entidad;
+
+public class Pokemon {
+    private int id;
+    private String name;
+    private String foto;
+    private String descripcion;
+
+
+    public Pokemon(int id, String name, String foto, String descripcion) {
+
+    }
+    public int getId(){ return id; }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+}
